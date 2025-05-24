98cf252a111f44075c15d1601c9f1c00b6ed3d5a
Gelber-23 <gemartinezpas@gmail.com>
2025-05-23 21:28:49 -0500

    added filter state

--- src/main/java/com/course/plazoleta/infraestructure/output/jpa/repository/IDishRepository.java
+++ src/main/java/com/course/plazoleta/infraestructure/output/jpa/repository/IDishRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IDishRepository extends JpaRepository<DishEntity, Long> {
-    Page<DishEntity> findByIdRestaurant_IdAndIdCategory_Id(Long idRestaurant, Long idCategory, Pageable pageable);
+    Page<DishEntity> findByIdRestaurant_IdAndIdCategory_IdAndActiveTrue(Long idRestaurant, Long idCategory, Pageable pageable);
 }
